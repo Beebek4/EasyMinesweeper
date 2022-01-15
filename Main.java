ad1b19fb36d3e5d7189722fd0967b95fc0963f46
Beebek4 <96885489+Beebek4@users.noreply.github.com>
2022-01-15 20:25:58 +0000

    Update Main.java
    Page layout and button changes imageview

--- Main.java
+++ Main.java
@@ -1,36 +1,40 @@
 package application;
-
+	
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
-public class Controller extends Application{
-	
-	@FXML
-	private Button button;
-	
-	@FXML
-	private ImageView imgview;
-	
-	public void initialize() {
-		Image image = new Image(getClass().getResourceAsStream("/images/smileyface.png")); 
-		imgview.setImage(image);
-		System.out.println("run");
-	}
-	
-	public void click (ActionEvent e) throws InterruptedException {
-		Image image = new Image(getClass().getResourceAsStream("/images/shockface.png"));
-		imgview.setImage(image);
-		System.out.println("set");
-	}
 
+public class Main extends Application {
+	
 	@Override
-	public void start(Stage arg0) throws Exception {
-		
+	public void start(Stage Stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			Stage.setScene(scene);
+			Stage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
+
 	
 }
